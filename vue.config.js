2e5fb559f10acb16e88c9cf53bf5cf9063434b8e
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-06-08 00:44:42 +0530

    updated vue config for pwa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/vue-weather-app/'
-        : '/'
+        : '/',
+
+    pwa: {
+        name: 'Vue Weather',
+        themeColor: '#673ab7',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+
+
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+
+            swSrc: 'dev/sw.js',
+
+        }
+    }
 }
