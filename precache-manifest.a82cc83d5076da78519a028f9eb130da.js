3748098c9f8335c63043c7e8dbe8beaeb9d2e87d
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-06-08 01:21:02 +0530

    deploy pwa

---
+++ precache-manifest.a82cc83d5076da78519a028f9eb130da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2256924b3c8ceaab6612",
+    "url": "/vue-weather-app/css/app.86b02417.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-weather-app/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "883cb00d86254cf485d5d5c4ed391c66",
+    "url": "/vue-weather-app/index.html"
+  },
+  {
+    "revision": "2256924b3c8ceaab6612",
+    "url": "/vue-weather-app/js/app.32b0b75b.js"
+  },
+  {
+    "revision": "d3b4b0ba90ab5eaccee4",
+    "url": "/vue-weather-app/js/chunk-vendors.3651e16f.js"
+  },
+  {
+    "revision": "7ecbba2477a7fcbb35a16e2fe626579f",
+    "url": "/vue-weather-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-weather-app/robots.txt"
+  }
+]);
